63d1e6d4139e2fd2f7f540faf929f9d5bc39e16e
Leelanjan <leelanjan@PWSLT-097.local>
2026-02-06 11:37:04 +0530

    updated

--- oops_assignments/python_assignment-4/5.py
+++ oops_assignments/python_assignment-4/5.py
@@ -1,27 +1,2 @@
 # 5) Write a Python class to find the three elements that sum to zero from a set of n real numbers. 
 # Input array : [-25, -10, -7, -3, 2, 4, 8, 10] Output : [[-10, 2, 8], [-7, -3, 10]].
-class ThreeSum:
-    def find_three_sum(self, nums):
-        nums.sort()
-        result = []
-        for i in range(len(nums) - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            left, right = i + 1, len(nums) - 1
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
-                if total < 0:
-                    left += 1
-                elif total > 0:
-                    right -= 1
-                else:
-                    result.append([nums[i], nums[left], nums[right]])
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-                    left += 1
-                    right -= 1
-        return result
-obj = ThreeSum()
-print(obj.find_three_sum([-25, -10, -7, -3, 2, 4, 8, 10]))
